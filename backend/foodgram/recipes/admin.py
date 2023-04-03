14fa1debc6750b4d46d06bbda9b1ea61d6ee3296
Oksana-laura <oksanashram@gmail.com>
2023-04-03 17:26:20 +0300

    правки

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -19,7 +19,9 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(models.Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'author', 'in_favorites')
+    list_display = ('pk', 'name', 'author', 'in_favorites',
+                    'cooking_time', 'text', 'tags', 'image'
+                    )
     list_editable = (
         'name', 'cooking_time', 'text', 'tags',
         'image', 'author'
